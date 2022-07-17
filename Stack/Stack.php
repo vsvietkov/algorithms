434934c2f2392fd756ba2efea77875b088bd21ed
Vitalii Svietkov <vitalii.svietkov>
2022-07-17 16:55:55 +0300

    Added a Stack

---
+++ Stack/Stack.php
@@ -0,0 +1,81 @@
+<?php
+
+/**
+ * Algorithm efficiency if O(1)
+ */
+class Stack
+{
+    private int   $size;
+    private int   $top  = -1;
+    private array $data = [];
+
+    /**
+     * @param int $size
+     */
+    public function __construct(int $size)
+    {
+        $this->size = $size < 0 ? 0 : $size;
+    }
+
+    /**
+     * Add an element to the top of a stack
+     *
+     * Returns true on success, false if the stack is already full
+     *
+     * @param mixed $value
+     * @return bool
+     */
+    public function push(mixed $value): bool
+    {
+        if ($this->isFull()) {
+            return false;
+        }
+        $this->data[] = $value;
+        $this->top++;
+
+        return true;
+    }
+
+    /**
+     * Remove an element from the top of a stack
+     */
+    public function pop(): mixed
+    {
+        if (!$this->isEmpty()) {
+            // The definition of the 'pop' stack method requires the check for the emptiness of data storage before
+            // operation execution to handle it and return NULL, for example, but in our case we do not need it
+            // as this check is done in the default 'array_pop' php function.
+            $this->top--;
+        }
+
+        return array_pop($this->data);
+    }
+
+    /**
+     * Check if the stack is empty
+     *
+     * @return bool
+     */
+    public function isEmpty(): bool
+    {
+        return $this->top === -1;
+    }
+
+    /**
+     * Check if the stack is full
+     *
+     * @return bool
+     */
+    public function isFull(): bool
+    {
+        return $this->size - 1 === $this->top;
+    }
+
+    /**
+     * Get the value of the top element without removing it
+     */
+    public function peek(): mixed
+    {
+        return $this->data[$this->top];
+    }
+}
---
+++ Stack/testingStack.php
@@ -0,0 +1,17 @@
+<?php
+require_once 'Stack.php';
+
+// Testing Stack - Reverse a work
+$word = readline('Enter a word to be reversed: ');
+echo "You entered $word\n";
+$stack = new Stack(strlen($word));
+
+foreach (str_split($word) as $letter) {
+    $stack->push($letter);
+}
+$reversedWord = '';
+
+while (!$stack->isEmpty()) {
+    $reversedWord .= $stack->pop();
+}
+echo "Reversed word: $reversedWord\n";
